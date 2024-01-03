6e74dc268e3c77bdf47052927c5e14fd75e43406
Emad Anwer <emadanwer.official@gmail.com>
2024-01-03 18:48:47 +0200

    [fix] task 2 - by adding error to the first then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'Success',
-    }))
+    .then(
+      () => ({
+        status: 200,
+        body: 'Success',
+      }),
+      () => Error(),
+    )
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
