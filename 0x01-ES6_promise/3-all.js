6bb310ab31482d32f5007d98f2a967d1ff1c1760
Emad Anwer <emadanwer.official@gmail.com>
2024-01-03 20:12:13 +0200

    [fix] task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
+  const promises = [createUser(), uploadPhoto()];
 
   return Promise.all(promises)
-    .then(([photoData, userData]) => {
+    .then(([userData, photoData]) => {
       console.log(photoData.body, userData.firstName, userData.lastName);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
