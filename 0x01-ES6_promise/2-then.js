c2085b906db82767157411d9100820f18e8a7893
Emad Anwer <emadanwer.official@gmail.com>
2024-01-03 18:57:22 +0200

    [fix] task 2 - check 2 "test add throw before error"

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,9 +5,10 @@ export default function handleResponseFromAPI(promise) {
         status: 200,
         body: 'Success',
       }),
-      () => Error(),
+      () => {
+        throw new Error();
+      },
     )
-    .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
