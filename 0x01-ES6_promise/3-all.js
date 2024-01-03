fd674d77258f47df9f81798fd9516b787cafdcca
Emad Anwer <emadanwer.official@gmail.com>
2024-01-03 20:20:27 +0200

    [fix] task 3 using ES6 ``

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  const promises = [uploadPhoto(), createUser()];
+
+  return Promise.all(promises)
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
