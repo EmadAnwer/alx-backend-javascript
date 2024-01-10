afca5821064dcc4fcb68666068d3b39ebdf5b0c5
Emad Anwer <emadanwer.official@gmail.com>
2024-01-10 21:56:17 +0200

    [add] task 4 StudentConstructor not working

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -37,3 +37,34 @@ const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
 };
 
 console.log(printTeacher('John', 'Doe'));
+interface StudentConstructor {
+  (firstName: string, lastName: string): string;
+}
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string): StudentConstructor {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student1 = new StudentClass('John', 'Doe');
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
+console.log(student1.firstName);
+console.log(student1.lastName);
