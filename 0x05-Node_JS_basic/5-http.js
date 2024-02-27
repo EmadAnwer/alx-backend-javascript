cf0b8f09e038300309083b8336fa3aa60d9b40d7
EmadAnwer <emadanwer.official@gmail.com>
2024-02-27 15:22:10 +0200

    [task 5]

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    // eslint-disable-next-line consistent-return
     fs.readFile(path, (error, dataBuffer) => {
       if (error) {
-        return reject();
+        reject(new Error('Cannot load the database')); // Provide a meaningful error message
+        return;
       }
       const data = dataBuffer.toString().split('\n');
       let count = 0;
@@ -14,11 +14,9 @@ function countStudents(path) {
 
       const firstnameIndex = data[0].split(',').indexOf('firstname');
       const fieldIndex = data[0].split(',').indexOf('field');
-      // eslint-disable-next-line no-plusplus
+
       for (let i = 1; i < data.length; i++) {
-        // eslint-disable-next-line no-continue
         if (data[i] === '') continue;
-        // eslint-disable-next-line no-plusplus
         count++;
         const row = data[i].split(',');
         if (fields[row[fieldIndex]]) {
@@ -27,41 +25,40 @@ function countStudents(path) {
           fields[row[fieldIndex]] = [row[firstnameIndex]];
         }
       }
+
       let studentsData = 'This is the list of our students\n';
       studentsData += `Number of students: ${count}\n`;
 
-      // eslint-disable-next-line guard-for-in
       for (const field in fields) {
         studentsData += `Number of students in ${field}: ${
           fields[field].length
-        }. List: ${fields[
-          field
-          // eslint-disable-next-line comma-dangle
-        ].join(', ')}\n`;
+        }. List: ${fields[field].join(', ')}\n`;
       }
+
       resolve(studentsData);
     });
-  }).catch(() => {
-    throw new Error('Cannot load the database');
   });
 }
+
 const app = http
-  // eslint-disable-next-line consistent-return
   .createServer((req, res) => {
     if (req.url === '/') {
-      return res.end('Hello Holberton School!');
-    }
-    if (req.url === '/students') {
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
       const path = process.argv[2];
 
       countStudents(path)
         .then((studentsData) => res.end(studentsData))
         .catch((error) => {
-          console.log(error);
+          console.error(error); // Log the error for debugging
+          res.statusCode = 500; // Internal Server Error
+          res.end('Internal Server Error');
         });
     } else {
-      return res.end();
+      res.statusCode = 404; // Not Found
+      res.end('404 Not Found');
     }
   })
   .listen(1245);
+
 module.exports = app;
