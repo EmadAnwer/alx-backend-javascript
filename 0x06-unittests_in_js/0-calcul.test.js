9300a0a7bfa18730385adfb0fb541e8010df1fee
EmadAnwer <emadanwer.official@gmail.com>
2024-02-29 12:43:54 +0200

    [task 0]

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('not rounded', () => {
-    assert.equal(calculateNumber(1, 1), 2);
-    assert.equal(calculateNumber(1, 3), 4);
+describe('calculateNumber', function () {
+  it('should return the sum of rounded numbers for integer inputs', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('has decimal point >= 0.5 ', () => {
-    assert.equal(calculateNumber(1.5, 1), 3);
-    assert.equal(calculateNumber(1, 2.6), 4);
-    assert.equal(calculateNumber(1.5, 2.6), 5);
-    assert.equal(calculateNumber(1.4, 2.6), 4);
+  it('should return the sum of rounded numbers for one integer and one float input', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('one arg has decimal point >= 0.5 ', () => {
-    assert.equal(calculateNumber(1.4, 1), 2);
-    assert.equal(calculateNumber(1, 2.1), 3);
-    assert.equal(calculateNumber(1.4, 1.4), 2);
+  it('should return the sum of rounded numbers for two float inputs', function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return NaN', () => {
-    assert.equal(calculateNumber(), NaN);
-    assert.equal(calculateNumber(1, 'e'), NaN);
-    assert.equal(calculateNumber('e', 'e'), NaN);
+  it('should return the sum of rounded numbers for two float inputs rounding up', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
